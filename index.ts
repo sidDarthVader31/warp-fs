42b6a51a8e50da687ef2cbda2e1f175e33918c7c
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-10-02 11:58:33 +0530

    add gitignore

--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
 import fs, { PathLike } from 'fs';
 import { Path } from './helpers/path';
 
-
-
-
-
 interface IWrapFs {
-
   fileExists(fileName: string): Promise<boolean>;
   readDir( path: PathLike, options:| {
                 encoding: BufferEncoding | null;
@@ -19,14 +14,13 @@ interface IWrapFs {
  ): Promise<Buffer>;
 }
  export class WrapFs implements IWrapFs {
-  private  readonly sharePath: string;
+  private  readonly sharePath: string
   private readonly path : Path
   constructor(sharePath: string){
     this.sharePath = sharePath;
     this.path = new Path(this.sharePath);
   }
   public readDir(path: PathLike, options: { encoding: BufferEncoding | null; withFileTypes?: false | undefined; recursive?: boolean | undefined; } | BufferEncoding | undefined | null): Promise<Buffer> {
-
   }
   public async fileExists(fileName: string): Promise<boolean> {
     const subDirectory = this.path.getSubDirectoryName(fileName);
